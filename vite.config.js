2ffb3b6bcf8ab7327dfb6f73f4f9036410ff8e3b
exi66 <eternityexi@gmail.com>
2026-03-28 18:30:13 +0400

    fix: basePath

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "mcgl-correct-evasion-calculator",
+  base: "/mcgl-correct-evasion-calculator",
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
